000260b8d56de47990cdba90563152f7a9849601
pengxianhong <290677893@qq.com>
2024-11-13 14:43:30 +0800

    First Commit

---
+++ DevKit/Views/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace DevKit.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var elements = new List<string>
+            {
+                "ADB", "APK", "TCP", "UDP", "WebSocket", "串口通信", "图标", "微软翻译", "转码", "颜色"
+            };
+        }
+    }
+}
