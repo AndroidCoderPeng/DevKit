72a5bd352f507f48d7968fb0ab65662c7f63b884
pengxianhong <290677893@qq.com>
2024-12-03 14:09:17 +0800

    完成颜色HEX与RGB相互转换

--- DevKit/ViewModels/ColorResourceViewModel.cs
+++ DevKit/ViewModels/ColorResourceViewModel.cs
@@ -1,9 +1,13 @@
-﻿using System.Windows;
+﻿using System.Drawing;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using DevKit.Utils;
 using HandyControl.Controls;
 using Prism.Commands;
 using Prism.Mvvm;
+using Color = System.Windows.Media.Color;
+using MessageBox = System.Windows.MessageBox;
 
 namespace DevKit.ViewModels
 {
@@ -16,6 +20,7 @@ namespace DevKit.ViewModels
         public DelegateCommand<NumericUpDown> ColorBlueValueChangedCommand { set; get; }
         public DelegateCommand<Slider> AlphaValueChangedCommand { set; get; }
         public DelegateCommand<string> CopyColorHexValueCommand { set; get; }
+        public DelegateCommand<string> ColorHexToRgbCommand { set; get; }
 
         #endregion
 
@@ -45,11 +50,44 @@ namespace DevKit.ViewModels
             get => _colorHexValue;
         }
 
+        private byte _red;
+
+        public byte Red
+        {
+            set
+            {
+                _red = value;
+                RaisePropertyChanged();
+            }
+            get => _red;
+        }
+
+        private byte _green;
+
+        public byte Green
+        {
+            set
+            {
+                _green = value;
+                RaisePropertyChanged();
+            }
+            get => _green;
+        }
+
+        private byte _blue;
+
+        public byte Blue
+        {
+            set
+            {
+                _blue = value;
+                RaisePropertyChanged();
+            }
+            get => _blue;
+        }
+
         #endregion
 
-        private byte _red;
-        private byte _green;
-        private byte _blue;
         private byte _alpha = 255;
 
         public ColorResourceViewModel()
@@ -62,6 +100,7 @@ namespace DevKit.ViewModels
             ColorBlueValueChangedCommand = new DelegateCommand<NumericUpDown>(ColorBlueValueChanged);
             AlphaValueChangedCommand = new DelegateCommand<Slider>(AlphaValueChanged);
             CopyColorHexValueCommand = new DelegateCommand<string>(CopyColorHexValue);
+            ColorHexToRgbCommand = new DelegateCommand<string>(ColorHexToRgb);
         }
 
         private void ColorRedValueChanged(NumericUpDown numeric)
@@ -117,6 +156,30 @@ namespace DevKit.ViewModels
         {
             Clipboard.SetText(colorVale);
             Growl.Success("颜色值已复制");
+        }
+
+        private void ColorHexToRgb(string colorValue)
+        {
+            if (!colorValue.StartsWith("#"))
+            {
+                MessageBox.Show("不是有效颜色值，无法转换", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (!colorValue.Substring(1, 6).IsHex())
+            {
+                MessageBox.Show("不是有效颜色值，无法转换", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            //转换RGB时，透明度默认为最大值255
+            _alpha = 255;
+
+            //#b1d85c
+            var color = ColorTranslator.FromHtml(colorValue);
+            Red = color.R;
+            Green = color.G;
+            Blue = color.B;
         }
     }
 }
