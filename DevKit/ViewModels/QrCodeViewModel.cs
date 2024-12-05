1b0e3ec1ccf4441d92ff6a93dbe56f08a9812733
pengxianhong <290677893@qq.com>
2024-12-05 22:32:39 +0800

    完成二维码生成，但是尺寸需要调整

--- DevKit/ViewModels/QrCodeViewModel.cs
+++ DevKit/ViewModels/QrCodeViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Media.Imaging;
+using DevKit.Utils;
 using Prism.Commands;
 using Prism.Mvvm;
+using ZXing;
+using ZXing.Common;
 
 namespace DevKit.ViewModels
 {
@@ -53,24 +57,50 @@ namespace DevKit.ViewModels
             ImageSelectedCommand = new DelegateCommand<Uri>(ImageSelected);
             ImageUnselectedCommand = new DelegateCommand(ImageUnselected);
         }
-        
-        private void UploadLogo(){}
-        
-        private void GenerateQrCode(string content){}
+
+        private void UploadLogo()
+        {
+        }
+
+        private void GenerateQrCode(string content)
+        {
+            if (string.IsNullOrWhiteSpace(content))
+            {
+                MessageBox.Show("请输入需要编码的内容", "温馨提示", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (content.Length > 512)
+            {
+                MessageBox.Show("内容过长，最大支持512个字符", "温馨提示", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var writer = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new EncodingOptions
+                {
+                    Width = 1024,
+                    Height = 1024
+                }
+            };
+            var qrCodeBitmap = writer.Write(content);
+            //需要重新规定尺寸
+            QrCodeBitmapImage = qrCodeBitmap.ToBitmapImage();
+        }
 
         private void SaveQrCode()
         {
-            
+            // qrCodeImage.Save("qrcode.png", System.Drawing.Imaging.ImageFormat.Png);
         }
-        
+
         private void ImageSelected(Uri uri)
         {
-            
         }
 
         private void ImageUnselected()
         {
-            
         }
     }
 }
