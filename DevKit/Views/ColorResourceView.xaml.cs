eb62d34de9dcefd152b5f9f13a63f2f968d8b4dd
pengxianhong <290677893@qq.com>
2025-05-27 15:55:40 +0800

    refactor(DevKit): 重构颜色资源视图
    - 优化了界面布局，使用 DockPanel 和 StackPanel 替代 Grid布局
    - 移除了不必要的嵌套层级，简化了 XAML 结构
    - 使用统一的样式和控件替代了自定义模板，提高了代码可读性
    - 增加了鼠标预览事件处理，为后续功能扩展做准备

--- DevKit/Views/ColorResourceView.xaml.cs
+++ DevKit/Views/ColorResourceView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+using DevKit.ViewModels;
+using Newtonsoft.Json;
 
 namespace DevKit.Views
 {
@@ -7,6 +11,16 @@ namespace DevKit.Views
         public ColorResourceView()
         {
             InitializeComponent();
+        }
+
+        private void ColorListBox_ListBoxItem_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is ListBoxItem item && item.Content is ColorResourceViewModel model)
+            {
+                var vm = DataContext as ColorResourceViewModel;
+                // vm?.ClientItemClickedCommand.Execute(model);
+                Console.WriteLine(JsonConvert.SerializeObject(model));
+            }
         }
     }
 }
