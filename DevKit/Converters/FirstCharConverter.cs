99bea7498776a0e62817464409ee8b6c765fbfe6
Pengxh <290677893@qq.com>
2026-09-26 13:10:36 +0800

    feat(ui): 优化应用包列表样式

---
+++ DevKit/Converters/FirstCharConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DevKit.Converters
+{
+    public class FirstCharConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var text = value as string;
+            return string.IsNullOrEmpty(text) ? string.Empty : text.Substring(0, 1).ToUpperInvariant();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
